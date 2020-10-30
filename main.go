72ed5967408ca156c967b5743cfb03e5e29ec8a7
Natalia Kirpa <66640604+nkirpa@users.noreply.github.com>
2020-10-30 11:39:51 +0400

    change hello text

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World"}
+		res := &response{Message: "Hello World. It's me, Natasha!"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
